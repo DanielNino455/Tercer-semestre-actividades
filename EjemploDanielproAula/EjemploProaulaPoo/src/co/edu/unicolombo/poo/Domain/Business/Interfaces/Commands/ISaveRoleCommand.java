e65000b9b71d0b51fe130eb320316e93dbb12452
Daniel.Niño.455 <159027510+DanielNino455@users.noreply.github.com>
2024-10-19 07:07:54 -0600

    Delete EjemploDanielproAula/EjemploProaulaPoo directory

--- EjemploDanielproAula/EjemploProaulaPoo/src/co/edu/unicolombo/poo/Domain/Business/Interfaces/Commands/ISaveRoleCommand.java
+++
@@ -1,8 +0,0 @@
-
-package co.edu.unicolombo.poo.Domain.Business.Interfaces.Commands;
-
-import co.edu.unicolombo.poo.Domain.Model.Role;
-
-public interface ISaveRoleCommand {
-  public int createRole(Role rol)throws Exception;  
-}
